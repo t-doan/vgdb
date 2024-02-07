b86c29cbb5d8bf5e8446356a3ecc184d8bf9c87e
Tai Doan <DoanTai11@gmail.com>
2024-02-07 18:09:49 +0000

    testing

--- client/src/GameDetail.tsx
+++ client/src/GameDetail.tsx
@@ -1,169 +1,169 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useEffect, useState } from 'react';
+// import { useLocation } from 'react-router-dom';
 
-type GameEntry = {
-  name: string;
-  id: number;
-  released: string;
-  background_image: string;
-  description_raw: string;
-  developers: string[];
-  publishers: string[];
-  genres: string[];
-  tags: string[];
-};
+// type GameEntry = {
+//   name: string;
+//   id: number;
+//   released: string;
+//   background_image: string;
+//   description_raw: string;
+//   developers: string[];
+//   publishers: string[];
+//   genres: string[];
+//   tags: string[];
+// };
 
-export function GameDetail() {
-  const { state } = useLocation();
-  const gameId = state.game.id;
-  const [game, onGame] = useState<GameEntry>();
-  const [error, setError] = useState<unknown>();
-  const [isLoading, setLoading] = useState<boolean>();
+// export function GameDetail() {
+//   const { state } = useLocation();
+//   const gameId = state.game.id;
+//   const [game, onGame] = useState<GameEntry>();
+//   const [error, setError] = useState<unknown>();
+//   const [isLoading, setLoading] = useState<boolean>();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`/api/details/${gameId}`);
-        if (!response.ok) {
-          throw new Error(`Error ${response.status}`);
-        }
-        const result = await response.json();
-        onGame(result);
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, [gameId]);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(`/api/details/${gameId}`);
+//         if (!response.ok) {
+//           throw new Error(`Error ${response.status}`);
+//         }
+//         const result = await response.json();
+//         onGame(result);
+//       } catch (error) {
+//         setError(error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+//     fetchData();
+//   }, [gameId]);
 
-  if (isLoading) return <div>Loading...</div>;
-  if (error)
-    return (
-      <div>
-        Error Loading Game {gameId}:{' '}
-        {error instanceof Error ? error.message : 'Unknown Error'}
-      </div>
-    );
-  if (!game) return null;
+//   if (isLoading) return <div>Loading...</div>;
+//   if (error)
+//     return (
+//       <div>
+//         Error Loading Game {gameId}:{' '}
+//         {error instanceof Error ? error.message : 'Unknown Error'}
+//       </div>
+//     );
+//   if (!game) return null;
 
-  return (
-    <div className=" px-48 mt-11">
-      <div className="row">
-        <h1 className="text-white font-bold text-6xl mb-11">{game.name}</h1>
-      </div>
-      <div className="row flex">
-        <div className="col-1 w-[762px] h-[428px]">
-          <img
-            className="w-[762px] h-[428px]"
-            src={game.background_image}
-            alt={game.name}
-          />
-        </div>
-        <div className="col-2 flex flex-wrap justify-evenly content-between">
-          <div>
-            <Developers developersList={game.developers} />
-          </div>
-          <div>
-            <Publishers publishersList={game.publishers} />
-          </div>
-          <div>
-            <h1 className="text-white text-2xl font-semibold">Released:</h1>
-            <h1 className="text-yellow-200 text-xl">{game.released}</h1>
-          </div>
-          <div>
-            <Genres genresList={game.genres} />
-          </div>
-          <div>
-            <Tags tagList={game.tags} />
-          </div>
-        </div>
-      </div>
-      <div className="pt-12">
-        <h1 className="text-white text-2xl font-semibold">Description</h1>
-        <h1 className="text-green-300 text-xl">{game.description_raw}</h1>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className=" px-48 mt-11">
+//       <div className="row">
+//         <h1 className="text-white font-bold text-6xl mb-11">{game.name}</h1>
+//       </div>
+//       <div className="row flex">
+//         <div className="col-1 w-[762px] h-[428px]">
+//           <img
+//             className="w-[762px] h-[428px]"
+//             src={game.background_image}
+//             alt={game.name}
+//           />
+//         </div>
+//         <div className="col-2 flex flex-wrap justify-evenly content-between">
+//           <div>
+//             <Developers developersList={game.developers} />
+//           </div>
+//           <div>
+//             <Publishers publishersList={game.publishers} />
+//           </div>
+//           <div>
+//             <h1 className="text-white text-2xl font-semibold">Released:</h1>
+//             <h1 className="text-yellow-200 text-xl">{game.released}</h1>
+//           </div>
+//           <div>
+//             <Genres genresList={game.genres} />
+//           </div>
+//           <div>
+//             <Tags tagList={game.tags} />
+//           </div>
+//         </div>
+//       </div>
+//       <div className="pt-12">
+//         <h1 className="text-white text-2xl font-semibold">Description</h1>
+//         <h1 className="text-green-300 text-xl">{game.description_raw}</h1>
+//       </div>
+//     </div>
+//   );
+// }
 
-type TagsProps = {
-  tagList: string[];
-};
-function Tags({ tagList }: TagsProps) {
-  const tag: JSX.Element[] = [];
-  for (let i = 0; i < tagList.length; i++) {
-    tag.push(
-      <li key={tagList[i]['id']} className="text-pink-300 text-xl">
-        {tagList[i]['name']}
-      </li>
-    );
-  }
-  return (
-    <>
-      <h1 className="text-white text-2xl font-semibold">Tags</h1>
-      <ul className="columns-3">{tag}</ul>
-    </>
-  );
-}
+// type TagsProps = {
+//   tagList: string[];
+// };
+// function Tags({ tagList }: TagsProps) {
+//   const tag: JSX.Element[] = [];
+//   for (let i = 0; i < tagList.length; i++) {
+//     tag.push(
+//       <li key={tagList[i]['id']} className="text-pink-300 text-xl">
+//         {tagList[i]['name']}
+//       </li>
+//     );
+//   }
+//   return (
+//     <>
+//       <h1 className="text-white text-2xl font-semibold">Tags</h1>
+//       <ul className="columns-3">{tag}</ul>
+//     </>
+//   );
+// }
 
-type GenreProps = {
-  genresList: string[];
-};
-function Genres({ genresList }: GenreProps) {
-  const genres: JSX.Element[] = [];
-  for (let i = 0; i < genresList.length; i++) {
-    genres.push(
-      <li key={genresList[i]['id']} className="text-red-500 text-xl">
-        {genresList[i]['name']}
-      </li>
-    );
-  }
-  return (
-    <ul>
-      <h1 className="text-white text-2xl font-semibold">Genres</h1>
-      {genres}
-    </ul>
-  );
-}
+// type GenreProps = {
+//   genresList: string[];
+// };
+// function Genres({ genresList }: GenreProps) {
+//   const genres: JSX.Element[] = [];
+//   for (let i = 0; i < genresList.length; i++) {
+//     genres.push(
+//       <li key={genresList[i]['id']} className="text-red-500 text-xl">
+//         {genresList[i]['name']}
+//       </li>
+//     );
+//   }
+//   return (
+//     <ul>
+//       <h1 className="text-white text-2xl font-semibold">Genres</h1>
+//       {genres}
+//     </ul>
+//   );
+// }
 
-type DevelopersProps = {
-  developersList: string[];
-};
-function Developers({ developersList }: DevelopersProps) {
-  const developers: JSX.Element[] = [];
-  for (let i = 0; i < developersList.length; i++) {
-    developers.push(
-      <li key={developersList[i]['id']} className="text-purple-400 text-xl">
-        {developersList[i]['name']}
-      </li>
-    );
-  }
-  return (
-    <ul>
-      <h1 className="text-white text-2xl font-semibold">Developers:</h1>
-      {developers}
-    </ul>
-  );
-}
+// type DevelopersProps = {
+//   developersList: string[];
+// };
+// function Developers({ developersList }: DevelopersProps) {
+//   const developers: JSX.Element[] = [];
+//   for (let i = 0; i < developersList.length; i++) {
+//     developers.push(
+//       <li key={developersList[i]['id']} className="text-purple-400 text-xl">
+//         {developersList[i]['name']}
+//       </li>
+//     );
+//   }
+//   return (
+//     <ul>
+//       <h1 className="text-white text-2xl font-semibold">Developers:</h1>
+//       {developers}
+//     </ul>
+//   );
+// }
 
-type PublishersProps = {
-  publishersList: string[];
-};
-function Publishers({ publishersList }: PublishersProps) {
-  const publishers: JSX.Element[] = [];
-  for (let i = 0; i < publishersList.length; i++) {
-    publishers.push(
-      <li key={publishersList[i]['id']} className="text-orange-400 text-xl">
-        {publishersList[i]['name']}
-      </li>
-    );
-  }
-  return (
-    <ul>
-      <h1 className="text-white text-2xl font-semibold"> Publishers:</h1>
-      {publishers}
-    </ul>
-  );
-}
+// type PublishersProps = {
+//   publishersList: string[];
+// };
+// function Publishers({ publishersList }: PublishersProps) {
+//   const publishers: JSX.Element[] = [];
+//   for (let i = 0; i < publishersList.length; i++) {
+//     publishers.push(
+//       <li key={publishersList[i]['id']} className="text-orange-400 text-xl">
+//         {publishersList[i]['name']}
+//       </li>
+//     );
+//   }
+//   return (
+//     <ul>
+//       <h1 className="text-white text-2xl font-semibold"> Publishers:</h1>
+//       {publishers}
+//     </ul>
+//   );
+// }
--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,120 +1,120 @@
-import { AppDrawer, Game } from './HomePage';
-import { Link, Outlet } from 'react-router-dom';
-import { SearchBar } from './SearchBar';
-import { Dispatch, SetStateAction, useState } from 'react';
-import { User } from './ProfilePage';
+// import { AppDrawer, Game } from './HomePage';
+// import { Link, Outlet } from 'react-router-dom';
+// import { SearchBar } from './SearchBar';
+// import { Dispatch, SetStateAction, useState } from 'react';
+// import { User } from './ProfilePage';
 
-type HeaderProps = {
-  user?: User;
-  setUser: (user?: User) => void;
-};
-export function Header({ user, setUser }: HeaderProps) {
-  const [searchResults, setSearchResults] = useState([]);
-  const [showResults, setShowResults] = useState<boolean>(false);
-  const handleSearch = (results) => {
-    setSearchResults(results);
-    setShowResults(true);
-  };
-  const logout = () => {
-    localStorage.removeItem('token');
-    setUser(undefined);
-  };
+// type HeaderProps = {
+//   user?: User;
+//   setUser: (user?: User) => void;
+// };
+// export function Header({ user, setUser }: HeaderProps) {
+//   const [searchResults, setSearchResults] = useState([]);
+//   const [showResults, setShowResults] = useState<boolean>(false);
+//   const handleSearch = (results) => {
+//     setSearchResults(results);
+//     setShowResults(true);
+//   };
+//   const logout = () => {
+//     localStorage.removeItem('token');
+//     setUser(undefined);
+//   };
 
-  return (
-    <>
-      <div className="Header w-[100%] h-[140px] bg-neutral-900 flex justify-evenly items-center relative">
-        <div className="Menu w-[140px] h-[63px] text-center text-white text-2xl font-normal flex justify-center items-center">
-          <div className="hover:text-red-600">
-            <AppDrawer />
-          </div>
-          <h1 className="Logo w-[140px] h-[63px] text-center text-white text-2xl font-normal flex justify-center items-center">
-            <Link to="/">VGDB</Link>
-          </h1>
-        </div>
-        <div className="SearchBar w-[720px] h-{63px] flex justify-center">
-          <h1 className="text-black text-opacity-40 text-[28px] font-bold">
-            <SearchBar onSearch={handleSearch} />
-            {showResults && (
-              <SearchResult
-                list={searchResults}
-                setShowResults={setShowResults}
-              />
-            )}
-          </h1>
-        </div>
-        <div className="UserCorner w-[264px] h-[63px] flex flex-wrap justify-around">
-          {user && (
-            <Link to="/user">
-              <div className="CornerPic w-[63px] h-[63px] bg-zinc-300 rounded-full">
-                <img
-                  className="w-[63px] h-[63px] rounded-full"
-                  src={user?.profileImage}
-                  alt="placeholder"
-                />
-              </div>
-            </Link>
-          )}
-          <div className="User w-[150px] h-[63px] flex flex-wrap justify-around">
-            {user && (
-              <>
-                <Link to="/user">
-                  <h1 className="UserText w-[164px] h-[42px] text-white text-[28px] font-bold hover:underline">
-                    My profile
-                  </h1>
-                </Link>
-                <h1
-                  onClick={logout}
-                  className="LogOut w-[72.47px] h-[21px] text-white text-lg font-normal hover:underline cursor-pointer">
-                  Log Out
-                </h1>
-              </>
-            )}
-            {!user && (
-              <div className="flex flex-col content-around items-center">
-                <Link to="/signup">
-                  <h1 className="text-white text-xl hover:underline">
-                    Sign Up
-                  </h1>
-                </Link>
-                <Link to="/login">
-                  <h1 className="text-white text-xl hover:underline">
-                    Sign In
-                  </h1>
-                </Link>
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-      <Outlet />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <div className="Header w-[100%] h-[140px] bg-neutral-900 flex justify-evenly items-center relative">
+//         <div className="Menu w-[140px] h-[63px] text-center text-white text-2xl font-normal flex justify-center items-center">
+//           <div className="hover:text-red-600">
+//             <AppDrawer />
+//           </div>
+//           <h1 className="Logo w-[140px] h-[63px] text-center text-white text-2xl font-normal flex justify-center items-center">
+//             <Link to="/">VGDB</Link>
+//           </h1>
+//         </div>
+//         <div className="SearchBar w-[720px] h-{63px] flex justify-center">
+//           <h1 className="text-black text-opacity-40 text-[28px] font-bold">
+//             <SearchBar onSearch={handleSearch} />
+//             {showResults && (
+//               <SearchResult
+//                 list={searchResults}
+//                 setShowResults={setShowResults}
+//               />
+//             )}
+//           </h1>
+//         </div>
+//         <div className="UserCorner w-[264px] h-[63px] flex flex-wrap justify-around">
+//           {user && (
+//             <Link to="/user">
+//               <div className="CornerPic w-[63px] h-[63px] bg-zinc-300 rounded-full">
+//                 <img
+//                   className="w-[63px] h-[63px] rounded-full"
+//                   src={user?.profileImage}
+//                   alt="placeholder"
+//                 />
+//               </div>
+//             </Link>
+//           )}
+//           <div className="User w-[150px] h-[63px] flex flex-wrap justify-around">
+//             {user && (
+//               <>
+//                 <Link to="/user">
+//                   <h1 className="UserText w-[164px] h-[42px] text-white text-[28px] font-bold hover:underline">
+//                     My profile
+//                   </h1>
+//                 </Link>
+//                 <h1
+//                   onClick={logout}
+//                   className="LogOut w-[72.47px] h-[21px] text-white text-lg font-normal hover:underline cursor-pointer">
+//                   Log Out
+//                 </h1>
+//               </>
+//             )}
+//             {!user && (
+//               <div className="flex flex-col content-around items-center">
+//                 <Link to="/signup">
+//                   <h1 className="text-white text-xl hover:underline">
+//                     Sign Up
+//                   </h1>
+//                 </Link>
+//                 <Link to="/login">
+//                   <h1 className="text-white text-xl hover:underline">
+//                     Sign In
+//                   </h1>
+//                 </Link>
+//               </div>
+//             )}
+//           </div>
+//         </div>
+//       </div>
+//       <Outlet />
+//     </>
+//   );
+// }
 
-type SearchResultsProps = {
-  list: Game[];
-  setShowResults: Dispatch<SetStateAction<boolean>>;
-};
-export function SearchResult({ list, setShowResults }: SearchResultsProps) {
-  const temp: JSX.Element[] = [];
-  for (let i = 0; i < list.length; i++) {
-    temp.push(
-      <Link
-        to={`/details/${list[i].id}`}
-        state={{ game: list[i] }}
-        key={list[i].id}>
-        <li className="hover:bg-yellow-200">{list[i].name}</li>
-      </Link>
-    );
-  }
+// type SearchResultsProps = {
+//   list: Game[];
+//   setShowResults: Dispatch<SetStateAction<boolean>>;
+// };
+// export function SearchResult({ list, setShowResults }: SearchResultsProps) {
+//   const temp: JSX.Element[] = [];
+//   for (let i = 0; i < list.length; i++) {
+//     temp.push(
+//       <Link
+//         to={`/details/${list[i].id}`}
+//         state={{ game: list[i] }}
+//         key={list[i].id}>
+//         <li className="hover:bg-yellow-200">{list[i].name}</li>
+//       </Link>
+//     );
+//   }
 
-  return (
-    <div
-      className="flex justify-center fixed top-0 left-0 right-0 bottom-0 z-50"
-      onClick={() => setShowResults(false)}>
-      <div className="w-[37.5rem] relative top-[6rem] right-[7.5rem]">
-        <ul className="bg-zinc-100">{temp}</ul>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div
+//       className="flex justify-center fixed top-0 left-0 right-0 bottom-0 z-50"
+//       onClick={() => setShowResults(false)}>
+//       <div className="w-[37.5rem] relative top-[6rem] right-[7.5rem]">
+//         <ul className="bg-zinc-100">{temp}</ul>
+//       </div>
+//     </div>
+//   );
+// }
